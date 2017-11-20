277a9be2a5ad882963edb87c58de419c72600ed5
Sakkaphong Luaengvilai <MaDonRa@Sakkaphongs-MacBook-Pro.local>
2017-11-20 16:49:25 +0700

    Initial Commit

---
+++ TestCoreData/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestCoreData
+//
+//  Created by Sakkaphong Luaengvilai on 11/20/2560 BE.
+//  Copyright © 2560 MaDonRa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
